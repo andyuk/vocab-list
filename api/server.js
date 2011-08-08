d5127e1706348b71935e434964663c5b56143140
Andrew Moist <andyjm@gmail.com>
2011-08-08 23:35:45 +0200

    progress on couch_db sync

--- api/server.js
+++ api/server.js
@@ -45,10 +45,54 @@ app.get(api_url, function(req, res, next){
 	res.send({"version": api_version});
 });
 
+
+
+var bulkDocsRequest = function(keys, callback) {
+
+	var options = {
+								port: db_port,
+								host: db_host,
+								method: 'POST',
+								path: '/' + db_name + '/_all_docs?include_docs=true',
+								headers: {
+										'Content-Type': 'application/json'	
+									}
+								};
+
+	var data = '{"keys":' + JSON.stringify(keys) + '}';
+	//console.log('Requesting new versions...');
+	//console.log(data);
+
+	var couch_request = http.request(options, function(response) {
+		
+		response.setEncoding('utf8');
+		
+		var buffer = '';
+		
+	  response.on('data', function (chunk) {
+			buffer += (chunk || '');
+	  });
+
+		var onEnd = function() {
+			
+			var json = JSON.parse(buffer);
+			
+			callback(json.rows);
+		};
+		
+	  response.on('close', onEnd);
+		response.on('end', onEnd);
+	});
+	couch_request.write(data);
+	couch_request.end();
+	
+};
+
+
 app.post(api_url + 'sync', function(req, res, next){
 	
 	var data = req.body.data;
-	console.log(req.body.data);
+	//console.log(req.body.data);
 	
 	if (data === undefined) {
 		
@@ -56,8 +100,6 @@ app.post(api_url + 'sync', function(req, res, next){
 		next();
 		return;
 	}
-	
-	//data = JSON.parse(data);
 	
 	var options = {
 								port: db_port,
@@ -69,22 +111,55 @@ app.post(api_url + 'sync', function(req, res, next){
 									}
 								};
 
-	var api_response = "";
-
 	var couch_request = http.request(options, function(response) {
 		
-	  //console.log('STATUS: ' + response.statusCode);
-	  //console.log('HEADERS: ' + JSON.stringify(response.headers));
 	  response.setEncoding('utf8');
+		var buffer = '';
 	
 	  response.on('data', function (chunk) {
-		
-			api_response = api_response + chunk;
-	    //console.log('BODY: ' + chunk);
+			buffer += (chunk || '');
 	  });
 	
 		var onEnd = function() {
-			res.send(api_response);
+			
+			// TODO: check if there are any conflicts, if so get the latest version
+			
+			var updates = JSON.parse(buffer);
+			
+			if (updates != null && updates.length > 0) {
+			
+				var updated_docs = [];
+
+				for (var key in updates) {
+					
+					//console.log(updates[key]);
+					//console.log(updates[key].id);
+					updated_docs.push(updates[key].id);
+				}
+				
+				bulkDocsRequest(updated_docs, function(latest_docs) {
+					
+					//console.log('got latest:');
+					//console.log(latest_docs);
+					for (var key in latest_docs) {
+						
+						for (var i in updates) {
+							
+							if (latest_docs[key].id === updates[i].id) {
+								updates[i] = latest_docs[key].doc;
+							}
+						}
+					}
+					//console.log('now updated:');
+					//console.log(updates);
+					
+					res.send(updates);
+					
+				});
+			} else {
+				
+				res.send(updates);
+			}
 		}
 	  response.on('close', onEnd);
 		response.on('end', onEnd);
